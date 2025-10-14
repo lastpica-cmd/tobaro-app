192b8a6dabf3ddc06ccec47fac6241e81a58d844
lastpica-cmd <lastpica@gmail.com>
2025-10-14 22:00:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -746,4 +746,5 @@ def _get_landuse_by_region_from_csv(region):
 
 if __name__ == "__main__":
     # For local run
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
