3b5d8daebe525fdc3418ad94f8ab8257244c44c3
lastpica-cmd <lastpica@gmail.com>
2025-10-14 23:43:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -747,4 +747,4 @@ def _get_landuse_by_region_from_csv(region):
 if __name__ == "__main__":
     # For local run
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)    
+    app.run(host="0.0.0.0", port=port, debug=False)
